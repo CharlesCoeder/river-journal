3a0df5f773b7685e6d42cea970726cf9fde395b0
CharlesCoeder <croederdev@gmail.com>
2025-05-12 14:00:25 -0400

    Add themes to config

--- packages/config/src/tamagui.config.ts
+++ packages/config/src/tamagui.config.ts
@@ -3,11 +3,13 @@ import { createTamagui } from 'tamagui'
 import { bodyFont, headingFont } from './fonts'
 import { animations } from './animations'
 import { tokens } from './tokens'
+import { themes } from './themes'
 
 export const config = createTamagui({
   ...defaultConfig,
   animations,
   tokens,
+  themes,
   fonts: {
     body: bodyFont,
     heading: headingFont,
