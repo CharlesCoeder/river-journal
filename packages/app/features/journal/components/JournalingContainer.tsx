19cbc04c071f83305ddcbee3791e7cd1375861e6
CharlesCoeder <croederdev@gmail.com>
2025-08-10 14:57:05 -0400

    Wrap editor in ScrollView

--- packages/app/features/journal/components/JournalingContainer.tsx
+++ packages/app/features/journal/components/JournalingContainer.tsx
@@ -1,5 +1,5 @@
 import type React from 'react'
-import { YStack } from '@my/ui'
+import { YStack, ScrollView } from '@my/ui'
 
 interface JournalingContainerProps {
   children: React.ReactNode
@@ -11,31 +11,33 @@ interface JournalingContainerProps {
  */
 export const JournalingContainer: React.FC<JournalingContainerProps> = ({ children }) => {
   return (
-    <YStack
-      flex={1}
-      backgroundColor="$background"
-      paddingHorizontal="$6"
-      paddingVertical="$4"
-      gap="$4"
-      width="100%"
-      alignSelf="stretch"
-      $sm={{
-        paddingHorizontal: '$4',
-        paddingVertical: '$3',
-        gap: '$3',
-      }}
-      $md={{
-        paddingHorizontal: '$6',
-        paddingVertical: '$5',
-        gap: '$5',
-      }}
-      $lg={{
-        paddingHorizontal: '$12',
-        paddingVertical: '$6',
-        gap: '$6',
-      }}
-    >
-      {children}
-    </YStack>
+    <ScrollView flex={1} width="100%" alignSelf="stretch" backgroundColor="$background">
+      <YStack
+        flex={1}
+        backgroundColor="$background"
+        paddingHorizontal="$6"
+        paddingVertical="$4"
+        gap="$4"
+        width="100%"
+        alignSelf="stretch"
+        $sm={{
+          paddingHorizontal: '$4',
+          paddingVertical: '$3',
+          gap: '$3',
+        }}
+        $md={{
+          paddingHorizontal: '$6',
+          paddingVertical: '$5',
+          gap: '$5',
+        }}
+        $lg={{
+          paddingHorizontal: '$12',
+          paddingVertical: '$6',
+          gap: '$6',
+        }}
+      >
+        {children}
+      </YStack>
+    </ScrollView>
   )
 }
