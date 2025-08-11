9cc4305ea1d90124d60480b6999d28a5db4f6d34
CharlesCoeder <croederdev@gmail.com>
2025-08-11 13:15:52 -0400

    Remove legend-state demo

--- packages/app/state/types.ts
+++ packages/app/state/types.ts
@@ -1,11 +1,3 @@
 /**
  * Core types for state management
  */
-
-// Demo state type for our demo feature
-export interface DemoState {
-  counter: number
-  text: string
-  lastUpdated: string | null
-  toggleState: boolean
-}
