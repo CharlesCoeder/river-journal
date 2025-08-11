3553d693fbc877dd337dd4db0922163e7dc8495b
CharlesCoeder <croederdev@gmail.com>
2025-08-11 15:09:46 -0400

    Persist theme settings

--- packages/app/state/theme/index.ts
+++ packages/app/state/theme/index.ts
@@ -1,4 +1,6 @@
 import { observable } from '@legendapp/state'
+import { syncObservable } from '@legendapp/state/sync'
+import { configurePersistence } from '../persistConfig'
 
 export type BaseThemeName = 'light' | 'dark'
 
@@ -26,6 +28,21 @@ export const theme$ = observable<ThemeState>({
   colorTheme: 'blue', // Default color
 })
 
+// Only setup persistence on the client side
+if (typeof window !== 'undefined') {
+  try {
+    syncObservable(
+      theme$,
+      configurePersistence({
+        persist: {
+          name: 'theme',
+        },
+      })
+    )
+  } catch (error) {
+    console.error('Error setting up journal store persistence:', error)
+  }
+}
 // Actions
 export const setBaseTheme = (baseTheme: BaseThemeName) => {
   theme$.baseTheme.set(baseTheme)
