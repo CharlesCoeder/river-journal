f2562a246cbe6cb6c2aec666a2dc1a61662c8019
CharlesCoeder <croederdev@gmail.com>
2026-03-25 08:58:31 -0400

    Fix framework runtime issues and add static CSS extraction

--- packages/config/src/tamagui.config.ts
+++ packages/config/src/tamagui.config.ts
@@ -14,3 +14,9 @@ export const config = createTamagui({
   animations,
   fonts,
 })
+
+export type Conf = typeof config
+
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends Conf {}
+}
